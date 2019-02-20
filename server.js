c7a621715794a2288dfedb75dc11759903a4055d
David Santiago Pulido Gómez <43011914+Dasapugo4444@users.noreply.github.com>
2019-02-20 11:25:38 -0500

    chore: se agrega favicon, se agrega libreria bro-wserify, babelify, se agrega index.js
    aaron tambien ayudo :D

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ var app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+
 app.get('/', function(req,res) {
   res.render('index');
 })
 
+app.get('/signup', function(req,res) {
+  res.render('index');
+})
 
+app.get('/signin', function(req,res) {
+  res.render('index');
+})
 
 app.listen(3000, function (err){
   if (err) return console.console.log('error'),process.exit(1);
